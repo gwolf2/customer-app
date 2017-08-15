94f7fc85fb4ac783e280202d7676e678cc26d59c
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-08-15 18:16:17 -0400

    Completed app

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs');
+var db = mongojs('customerapp', ['users']);
+var ObjectId = mongojs.ObjectId;
 
 var app = express();
 
@@ -24,31 +28,79 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
 
-var users = [
-	{
-		id: 1,
-		first_name: 'John',
-		last_name: 'Doe',
-		email: 'johndoe@myspace.com'
-	},
-	{
-		id: 2,
-		first_name: 'James',
-		last_name: 'Bond',
-		email: 'jamesbondjamesbond@jamesbond.com'
-	},
-			{
-		id: 1,
-		first_name: 'Foofy',
-		last_name: 'McDoofy',
-		email: 'foofsmcdoofs@aol.com'
-	},
-]
+// Global Vars
+app.use(function(req, res, next) {
+	res.locals.errors = null;
+	next();
+});
 
-app.get('/', function(req, res) {	
-	res.render('index', {
-		title: 'Customers',
-		users: users
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.get('/', function(req, res) {
+	db.users.find(function (err, docs) {
+		console.log(docs);
+		res.render('index', {
+			title: 'Customers',
+			users: docs
+		});		
+	})
+});
+
+app.post('/users/add', function(req, res) {
+
+	req.checkBody('first_name', 'First Name is Required').notEmpty();;
+	req.checkBody('last_name', 'Last Name is Required').notEmpty();;
+	req.checkBody('email', 'Email is Required').notEmpty();;
+
+	var errors = req.validationErrors();
+
+	if (errors) {
+		res.render('index', {
+			title: 'Customers',
+			users: users,
+			errors: errors
+		});
+	} else {
+		var newUser = {
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		email: req.body.email
+		}
+
+		db.users.insert(newUser, function(err, result) {
+			if (err) {
+				console.log(err);
+			} else {
+				res.redirect('/');
+			}
+		});
+
+		console.log('SUCCESS');
+	}
+});
+
+app.delete('/users/delete/:id', function(req, res) {
+	db.users.remove({_id: ObjectId(req.params.id)}, function(err) {
+		if (err) {
+			console.log(err);
+		}
+		res.redirect('/');
 	});
 });
 
